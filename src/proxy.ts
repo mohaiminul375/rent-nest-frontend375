09c78fc60c49515db3ed660d8b0770e653629715
Mohaiminul375 <mohaiminul375@gmail.com>
2026-09-10 21:29:38 +0600

    fix: fix invalid signature issue

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from 'next/server'
 import { JwtPayload } from 'jsonwebtoken'
 import { jwtUtils } from './utils/jwt'
 import { cookies } from 'next/headers'
+import { getNewAccessToken } from './service/refreshToken'
 
 // Routes
 const AUTH_ROUTE = ["/login", "/register"]
@@ -13,15 +14,37 @@ export async function proxy(request: NextRequest) {
     // return NextResponse.redirect(new URL('/home', request.url))
     const pathName = request.nextUrl.pathname;
     const cookieStore = await cookies();
-    const accessToken = request.cookies.get("accessToken")?.value;
-    const decodedAccessToken = accessToken ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string) : null;
+
+    let accessToken = request.cookies.get("accessToken")?.value;
+    const refreshToken = request.cookies.get("refreshToken")?.value;
+
+    let decodedAccessToken = accessToken ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string) : null;
+    const decodedRefreshToken = refreshToken ? jwtUtils.verifyToken(refreshToken, process.env.JWT_ACCESS_SECRET as string) : null;
+
+    if (!decodedAccessToken?.success && decodedRefreshToken?.success) {
+        //access token has expired but refresh token is valid, get new access token from backend
+        const result = await getNewAccessToken();
+
+        if (result.success) {
+            const newAccessToken = result.data.accessToken;
+
+            cookieStore.set("accessToken", newAccessToken, {
+                httpOnly: true,
+                maxAge: 60 * 60 * 24,
+                sameSite: "lax",
+            });
+
+            accessToken = newAccessToken;
+            decodedAccessToken = jwtUtils.verifyToken(accessToken!, process.env.JWT_ACCESS_SECRET as string);
+        }
+    }
 
     let userRole = null;
 
-    // if (!decodedToken?.success) {
-    //     cookieStore.delete("accessToken");
-    //     // return NextResponse.redirect(new URL('/login', request.url))
-    // }
+    if (!decodedAccessToken?.success) {
+        //token has expired or is invalid, clear the cookies
+        cookieStore.delete("accessToken");
+    }
 
 
     if (decodedAccessToken?.success && decodedAccessToken.data) {
